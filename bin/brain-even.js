40dc500bd99c15da3c5d3ca7458f6f1e4928fcd1
Olga Soboleva <helgasn@gmail.com>
2021-11-28 13:51:37 +0300

    add game brain-even

---
+++ bin/brain-even.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import { dialog } from '../src/even-games.js';
+
+dialog();
--- src/even-games.js
+++ src/even-games.js
@@ -1,7 +1,31 @@
 import readlineSync from 'readline-sync';
 
+const getRandomInt = (min, max) => {
+  const min1 = Math.ceil(min);
+  const max1 = Math.floor(max);
+  return Math.floor(Math.random() * (max1 - min1)) + min1;
+};
 export const dialog = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
+  let randomNum;
+  const num = 3;
+  for (let i = 1; i <= num; i += 1) {
+    randomNum = getRandomInt(0, 100000);
+    console.log('Answer "yes" if the number is even, otherwise answer "no".');
+    console.log('Question: ', randomNum);
+    const correctAnswer = randomNum % 2 === 0 ? 'yes' : 'no';
+    const answer = readlineSync.question('Your answer: ');
+    if (answer === correctAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${name}!`);
+      break;
+    }
+    if (i === num) {
+      console.log(`Congratulations, ${name}!`);
+    }
+  }
 };
